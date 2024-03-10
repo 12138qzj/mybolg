819a5be27ca60dc7a57c02ab8e69ec5ab4da4290
12138qzj <2442319906@qq.com>
2024-03-10 19:54:42 +0800

    修改

--- myblog/deploy.sh
+++ myblog/deploy.sh
@@ -24,7 +24,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/12138qzj/12138qzj.github.io.git master
+git push -f https://github.com/12138qzj/12138qzj.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
